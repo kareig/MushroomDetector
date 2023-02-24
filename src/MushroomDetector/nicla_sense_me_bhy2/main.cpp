e23e4eea2fec102f907e3b039572ffd4f4c60198
kareig <karolis.eigelis@gmail.com>
2023-02-24 10:44:10 +0200

    added max val for mushrooms
    Former-commit-id: d46d88b7d1e1864f03ab1104a321bbeda01b22dd

--- src/MushroomDetector/nicla_sense_me_bhy2/main.cpp
+++ src/MushroomDetector/nicla_sense_me_bhy2/main.cpp
@@ -6,17 +6,17 @@
 
 long previousMillisBat = 0;
 int interval = 10000;
-void updateLEDBatteryStatus(bool interval, int ms=1000);
+void updateLEDBatteryStatus(bool interval, int ms = 1000);
 void setLEDBatteryStatus();
 
 void setup()
 {
   Serial.begin(115200);
 
-#if DEBUG  
+#if DEBUG
   BHY2.debug(Serial);
 #endif
-  
+
   nicla::begin();
   nicla::leds.begin();
   nicla::enableCharge(100);
@@ -26,32 +26,45 @@ void setup()
 
 void loop()
 {
-    BHY2.update();
-    updateLEDBatteryStatus(true,interval); 
+  BHY2.update();
+  updateLEDBatteryStatus(true, interval);
 }
 
-void updateLEDBatteryStatus(bool interval, int ms) {
-    unsigned long currentMillis = millis();
-    if(!interval) {
+void updateLEDBatteryStatus(bool interval, int ms)
+{
+  unsigned long currentMillis = millis();
+  if (!interval)
+  {
+    setLEDBatteryStatus();
+    return;
+  }
+  else
+  {
+    if (currentMillis - previousMillisBat >= ms)
+    {
+      previousMillisBat = currentMillis;
       setLEDBatteryStatus();
-      return;
-    } else {
-      if(currentMillis - previousMillisBat >= ms) {
-        previousMillisBat = currentMillis;
-        setLEDBatteryStatus();
-      }
     }
+  }
 }
 
-void setLEDBatteryStatus() {
+void setLEDBatteryStatus()
+{
   int batteryStatus = nicla::getBatteryStatus();
-  if (batteryStatus == 5 || batteryStatus == 4 ) {
-          nicla::leds.setColor(green); 
-      } else if (batteryStatus == 3) {
-          nicla::leds.setColor(yellow);
-      } else if (batteryStatus == 2) {
-          nicla::leds.setColor(red);
-      } else {
-          nicla::leds.setColor(off);  
-      }
+  if (batteryStatus == 5 || batteryStatus == 4)
+  {
+    nicla::leds.setColor(green);
+  }
+  else if (batteryStatus == 3)
+  {
+    nicla::leds.setColor(yellow);
+  }
+  else if (batteryStatus == 2)
+  {
+    nicla::leds.setColor(red);
+  }
+  else
+  {
+    nicla::leds.setColor(off);
+  }
 }
